97ea618dcfcc4bb20fe6c5388240163c0de2ea5e
Rejna <klucha1234@o2.pl>
2013-06-12 00:29:01 +0200

    Update SummonerRating.js
    Quick fix for "api error" posted lately. Both js and c# codes need some review about what data we can fetch from Riot and what we record locally.

--- Web/Script/Module/SummonerRating.js
+++ Web/Script/Module/SummonerRating.js
@@ -1,67 +1,51 @@
-function getRatingTable(region, summoner, statistics)
+function getRatingTable(region, summoner, statistics) 
 {
     var ratings = statistics.Ratings;
-
-    var columnTitles =
-        [
-            'Map',
-            'Mode',
-            'Games',
-            'W',
-            'L',
-            'W - L',
-            'WR',
-            'Left',
-            'Rating',
-            'Top rating',
-        ];
-
+    var columnTitles = 
+    [
+        'Map',
+        'Mode',
+        'Games',
+        'W',
+        'L',
+        'W - L',
+        'WR',
+        'Left',
+        'Rating',
+        'Top rating',
+    ];
     var output = table();
     output.id = 'ratingTable';
-
     var tableCaption = caption('General Statistics');
     tableCaption.className = 'profile';
     output.add(tableCaption);
-
     output.add(getTableHeadRow(columnTitles));
-
     var rowCount = 0;
     ratings.forEach(function(rating) {
         var gamesPlayed = rating.Wins + rating.Losses;
         if (gamesPlayed == 0)
             return;
         var map;
-        if(rating.Map == 1 && rating.GameMode == 2)
+        if (rating.Map == 1 && rating.GameMode == 2) 
         {
-            //This is not actually Unranked Summoner's Rift data but collective data for both Summoner's Rift and Twisted Treeline
             map = "Summoner's Rift/Twisted Treeline";
-        }
+        } 
         else
             map = getMapString(rating.Map);
-        var mapLink = anchor(map, function() { system.graphHandler.open(region, summoner.AccountId, rating.Map, rating.GameMode); });
+        var mapLink = anchor(map, function() {
+            system.graphHandler.open(region, summoner.AccountId, rating.Map, rating.GameMode);
+        });
+        var fields = 
+        [
+            [mapLink, false], 
+            [getGameModeString(rating.GameMode), false], 
+            [gamesPlayed, true], 
+            [rating.Wins, true]
+        ];
 
-        var fields =
-            [
-                [mapLink, false],
-                [getGameModeString(rating.GameMode), false],
-                [gamesPlayed, true],
-            ];
-
-        if(rating.GameMode == 3 || rating.GameMode == 4)
-        {
-            fields = fields.concat([
-                [rating.Wins, true],
-                [rating.Losses, true],
-                [signum(rating.Wins - rating.Losses), true],
-                [percentage(rating.Wins / (rating.Wins + rating.Losses)), true],
-            ]);
-        }
-        else
-        {
-            for(var i = 0; i < 4; i++)
-                fields.push(['-', true]);
-        }
-
+        for(var i = 0; i < 3; i++)
+            fields.push(['-', true]);
+        
         fields = fields.concat([
             [rating.Leaves, true],
             [getCurrentRating(rating), true],
@@ -72,14 +56,14 @@ function getRatingTable(region, summoner, statistics)
             var content = field[0];
             var isNumeric = field[1];
             var cell = tableCell(content);
-            if(isNumeric)
+            if (isNumeric)
                 cell.className = 'numeric';
             row.add(cell);
         });
         output.add(row);
         rowCount++;
     });
-    if(rowCount > 0)
+    if (rowCount > 0)
         return output;
     else
         return '';
